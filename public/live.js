b8ec16a79bd5194af1f699652d1025420466915e
Ben Zhang <benzh@cs.berkeley.edu>
2017-02-22 15:38:53 -0800

    feat: metricgraphics tested

--- public/live.js
+++ public/live.js
@@ -8,3 +8,18 @@ exampleSocket.onmessage = function (event) {
   console.log("Server responds: " + event.data);
 }
 
+d3.json('fake_users1.json', function(data) {
+    data = MG.convert.date(data, 'date');
+    MG.data_graphic({
+        title: "Line Chart",
+        description: "This is a simple line chart.",
+        area: false,
+        data: data,
+        width: 600,
+        height: 200,
+        right: 40,
+        target: document.getElementById('fake_users1'),
+        x_accessor: 'date',
+        y_accessor: 'value'
+    });
+});
