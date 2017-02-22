2551a3341c8d73ab75160b220d3a563ad2411258
Ben Zhang <benzh@cs.berkeley.edu>
2017-02-22 15:59:57 -0800

    feat: simple demo that updates plots

--- public/live.js
+++ public/live.js
@@ -1,15 +1,14 @@
-var exampleSocket = new WebSocket("ws://127.0.0.1:3012");
+var data = [];
+var i = 0;
+var interval;
+interval = setInterval(function() {
+    var date = new Date();
+    var value = Math.sin(i / 100 * 2 * Math.PI);
+    i++;
+    data.push({'date': date, 'value': value});
+}, 100);
 
-exampleSocket.onopen = function (event) {
-    exampleSocket.send("Hello World");
-};
-
-exampleSocket.onmessage = function (event) {
-  console.log("Server responds: " + event.data);
-}
-
-d3.json('fake_users1.json', function(data) {
-    data = MG.convert.date(data, 'date');
+setInterval(function() {
     MG.data_graphic({
         title: "Line Chart",
         description: "This is a simple line chart.",
@@ -22,4 +21,18 @@ d3.json('fake_users1.json', function(data) {
         x_accessor: 'date',
         y_accessor: 'value'
     });
-});
+}, 2000);
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Socket communication
+//
+////////////////////////////////////////////////////////////////////////////////
+var exampleSocket = new WebSocket("ws://127.0.0.1:3012");
+exampleSocket.onopen = function (event) {
+    exampleSocket.send("Hello World");
+};
+
+exampleSocket.onmessage = function (event) {
+  console.log("Server responds: " + event.data);
+}
