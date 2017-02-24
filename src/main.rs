93e043f99bf23dd806e5137d15380d4728e503eb
Ben Zhang <benzh@cs.berkeley.edu>
2017-02-23 16:50:37 -0800

    fix: s/metric/metrics

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 extern crate rand;
-extern crate metric_graphics;
+extern crate metrics_graphics;
 use rand::Rng;
 
 fn main() {
-    let mut ss = metric_graphics::init("127.0.0.1:3000");
+    let mut ss = metrics_graphics::init("127.0.0.1:3000");
     let mut rng = rand::thread_rng();
     loop {
         ::std::thread::sleep(::std::time::Duration::from_secs(1));
